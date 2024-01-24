78d0f9688ee9f7ce91697a995cee3e4dc5397af5
Goldeninscriptions <12232444@mail.sustech.edu.cn>
2024-01-24 21:14:26 +0800

    Debug

--- Analysis/EssBC.m
+++ Analysis/EssBC.m
@@ -1,12 +1,13 @@
-function [K, F] = EssBC(K, F, total_dof, BCs)
+function [K, F] = EssBC(K, F, total_dof, Nodes, BCs, u_exact)
 % Essential BC
 
 % Number of Dirichlet nodes
 num_DN = length(BCs.Dirichlet.DN);
 for nn = 1 : num_DN
     DiriNode = BCs.Dirichlet.DN(nn);
+    node_xyz = Nodes(:, DiriNode);
 
-    F(DiriNode) = 0.0;
+    F(DiriNode) = u_exact(node_xyz(1), node_xyz(2));
     K(:, DiriNode) = zeros(total_dof, 1);
     K(DiriNode, :) = zeros(1, total_dof);
     K(DiriNode, DiriNode) = 1.0;
